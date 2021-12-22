e1c3b5585402317197207077ae241973b43879b8
Badee-Salloum <bade33sm@gmail.com>
2021-12-22 13:37:09 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/rosa1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rosa1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:rosa1/ProductDetail.dart';
+import 'package:flutter/material.dart';
+import 'CategoriesScreen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Flowers',
+    home: ProductDetail(),
+  ));
+}
